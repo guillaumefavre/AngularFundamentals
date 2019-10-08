0221efdc062d64f14a5d2f066bc57cc77e7e6d54
Guillaume FAVRE <guillaumefavre56@gmail.com>
2019-10-08 18:14:34 +0200

    Routing part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,20 @@ import { EventsThumbnailComponent } from './events/events-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
 import { EventService } from './events/shared/event.Service';
 import { ToastrService } from './common/toastr.service';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { appRoutes } from './routes';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventsThumbnailComponent,
+    EventDetailsComponent,
     NavBarComponent
   ],
   providers: [EventService, ToastrService],

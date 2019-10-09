87d577d22336720b7370d8c2290f4849526673db
Guillaume FAVRE <guillaumefavre56@gmail.com>
2019-10-09 17:10:21 +0200

    Forms : custom validators

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -5,13 +5,15 @@ import {
     EventDetailsComponent,
     CreateEventComponent,
     EventRouteActivator,
-    EventListResolver
+    EventListResolver,
+    CreateSessionComponent
 } from './events/index'
 
 import { Error404Component } from './errors/404.component';
 
 export const appRoutes: Routes = [
     { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent']},
+    { path: 'events/session/new', component: CreateSessionComponent},
     { path: 'events', component: EventsListComponent, resolve: {events: EventListResolver} },
     { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator]},
     { path: '404', component: Error404Component},

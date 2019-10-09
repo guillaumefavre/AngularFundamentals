a515213a3e3c1dd65b0f3020d52200c196aff6de
Guillaume FAVRE <guillaumefavre56@gmail.com>
2019-10-09 17:43:31 +0200

    Communicating between components

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
 import { ISession, restrictedWords } from '../shared';
 
+
 @Component({
+    selector: 'create-session',
     templateUrl: './create-session.component.html',
     styles:[`
         em { float:right; color: #E05C65; padding-left: 10px;}
@@ -15,6 +17,8 @@ import { ISession, restrictedWords } from '../shared';
 })
 export class CreateSessionComponent implements OnInit {
 
+    @Output() saveNewSession = new EventEmitter();
+    @Output() cancelAddSession = new EventEmitter();
     newSessionForm: FormGroup;
     name: FormControl;
     presenter: FormControl;
@@ -52,5 +56,10 @@ export class CreateSessionComponent implements OnInit {
             voters: []
         }
         console.log(session);
+        this.saveNewSession.emit(session);
+    }
+
+    cancel() {
+        this.cancelAddSession.emit();
     }
 }
